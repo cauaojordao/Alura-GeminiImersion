20ff7ed0c137887bfa4c7d3ecc1b8332ff78a606
cauaojordao <cauaojordao@gmail.com>
2024-12-03 17:23:28 -0300

    Lesson 5 finished

--- instalike-backend/src/models/postModel.js
+++ instalike-backend/src/models/postModel.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { ObjectId } from "mongodb";
 import { connectToDB } from "../db.js";
 
--- instalike-backend/src/routes/postsRoutes.js
+++ instalike-backend/src/routes/postsRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import multer from "multer";
+import cors from "cors";
 import {
   listPosts,
   newPost,
   updateNewPost,
   uploadImage,
 } from "../controllers/postsController.js";
+
+const corsOptions = {
+  origin: "http://localhost:8000",
+  optionsSuccessStatus: 200,
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -20,6 +26,7 @@ const upload = multer({ dest: "./uploads/", storage });
 
 export const routes = async (app) => {
   app.use(express.json());
+  app.use(cors(corsOptions));
   app.get("/posts", listPosts);
   app.post("/posts", newPost);
   app.post("/upload", upload.single("image"), uploadImage);
